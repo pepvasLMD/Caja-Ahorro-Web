2685a1ff23a6d123083fe7fe96a8ccdb044ea18d
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-06-09 16:03:55 -0500

    Agregando Dar Aportacion

---
+++ CajaAhorro.WEBSERVICE/CajaAhorro.ENTITY/Parametros/ENTransaccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajaAhorro.ENTITY.Parametros
+{
+    public class ENTransaccion
+    {
+        public double monto { get; set; }
+
+        public int idcuenta { get; set; }
+    }
+}
