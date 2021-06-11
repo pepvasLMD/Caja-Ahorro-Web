120b6a384aeef8ab37e51d3fb1f36604ea7d52d5
Cesar73 <cesar_7391@outlook.com>
2021-06-11 12:04:07 -0500

    Vista Préstamo

---
+++ CajaAhorro.WEB/CajaAhorro.WEB/Controllers/PrestamoController.cs
@@ -0,0 +1,35 @@
+﻿using CajaAhorro.WEB.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajaAhorro.WEB.Controllers
+{
+    public class PrestamoController : Controller
+    {
+        // GET: Prestamo
+        public ActionResult Prestamo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult validarAccesoModulo()
+        {
+            var session = Session.GetCurrentUser();
+
+            if (session.tipo == "cliente")
+            {
+                var respuesta = "ok";
+                return Json(new { dt = respuesta });
+            }
+            else
+            {
+                var respuesta = "error";
+                return Json(new { dt = respuesta });
+            }
+        }
+    }
+}
--- CajaAhorro.WEB/CajaAhorro.WEB/Scripts/app/panel.js
+++ CajaAhorro.WEB/CajaAhorro.WEB/Scripts/app/panel.js
@@ -1,5 +1,4 @@
 ﻿
-
 function transacciones() {
     Post("Transacciones/validarAccesoModulo").done(function (datos) {
 
@@ -20,41 +19,20 @@ function transacciones() {
         }
     })
 }
-/*
-function empleados() {
-    Post("Empleados/validarAccesoModulo").done(function (datos) {
 
-        if (datos.dt == "ok") {
-
-            window.location = fnBaseURLWeb("Empleados/Empleados");
-
-        } else {
-            swal({
-                position: 'top-end',
-                type: 'error',
-                title: 'No tiene acceso a este módulo',
-                text: 'Contacte con el administrador del sistema',
-                showConfirmButton: true,
-                timer: 60000,
-                confirmButtonText: 'Cerrar'
-            })
-        }
-    })
-}
-
-function proveedores() {
-    Post("Proveedores/validarAccesoModulo").done(function (datos) {
+function prestamos() {
+    Post("Prestamo/validarAccesoModulo").done(function (datos) {
 
         if (datos.dt == "ok") {
             //LA URL A LA QUE LLEVA
-            window.location = fnBaseURLWeb("Proveedores/Proveedores");
+            window.location = fnBaseURLWeb("Prestamo/Prestamo");
 
         } else {
             swal({
                 position: 'top-end',
                 type: 'error',
                 title: 'No tiene acceso a este módulo',
-                text: 'Contacte con el administrador del sistema',
+                text: 'Sólo para clientes',
                 showConfirmButton: true,
                 timer: 60000,
                 confirmButtonText: 'Cerrar'
@@ -63,45 +41,7 @@ function proveedores() {
     })
 }
 
-function productos() {
 
-    Post("Productos/validarAccesoModulo").done(function (datos) {
 
-        if (datos.dt == "ok") {
-            window.location = fnBaseURLWeb("Productos/Productos");
-        } else {
-            swal({
-                position: 'top-end',
-                type: 'error',
-                title: "No tiene acceso a este módulo",
-                text: 'Contacte con el administrador del sistema',
-                showConfirmButton: true,
-                timer: 60000,
-                confirmButtonText: 'Cerrar'
-            })
-        }
-    })
-}
 
-//Lo que hace referencia en el panel
-function inventario() {
-
-    Post("Productos/validarAccesoModulo").done(function (datos) {
-
-        if (datos.dt == "ok") {
-            window.location = fnBaseURLWeb("Inventarios/Inventarios");
-        } else {
-            swal({
-                position: 'top-end',
-                type: 'error',
-                title: "No tiene acceso a este módulo",
-                text: 'Contacte con el administrador del sistema',
-                showConfirmButton: true,
-                timer: 60000,
-                confirmButtonText: 'Cerrar'
-            })
-        }
-    })
-}
-*/
 
