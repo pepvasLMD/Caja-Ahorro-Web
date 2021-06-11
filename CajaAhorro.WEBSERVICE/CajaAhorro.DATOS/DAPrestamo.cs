21e84d58dd75aba0a95b85ebcb8b5f405021510b
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-06-11 15:47:19 -0500

    Creando controlador Prestamo

---
+++ CajaAhorro.WEBSERVICE/CajaAhorro.DATOS/DAPrestamo.cs
@@ -0,0 +1,52 @@
+﻿using CajaAhorro.ENTITY.Parametros;
+using CajaAhorro.ENTITY.Response;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajaAhorro.DATOS
+{
+    public class DAPrestamo
+    {
+        public List<ResponseUsuario> getPrestar(ENPrestamo paramss)
+        {
+            try
+            {
+                string cs = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+                var lista = new List<ResponseUsuario>();
+
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    conn.Open();
+                    //Se llama al store procedure
+                    SqlCommand cmd = new SqlCommand("usp_darPrestamo", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@idusuario", paramss.idusuario));
+                    cmd.Parameters.Add(new SqlParameter("@dinero", paramss.dinero));
+                    cmd.Parameters.Add(new SqlParameter("@opcion", paramss.opcion));
+
+                    //Ejecuta el SP
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            var result = new ResponseUsuario();
+                            result.response = Convert.ToString(rdr["response"]);
+                            lista.Add(result);
+                        }
+                    }
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CajaAhorro.WEBSERVICE/CajaAhorro.WEBSERVICE/Controllers/PrestamoController.cs
@@ -0,0 +1,36 @@
+﻿using CajaAhorro.DATOS;
+using CajaAhorro.ENTITY.Parametros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CajaAhorro.WEBSERVICE.Controllers
+{
+    [RoutePrefix("api/Prestamo")]
+    public class PrestamoController : ApiController
+    {
+        private DAPrestamo daPrestamo;
+
+        public PrestamoController()
+        {
+            daPrestamo = new DAPrestamo();
+        }
+
+        [HttpPost]
+        [Route("prestar")]
+        public IHttpActionResult Prestar(ENPrestamo paramss)
+        {
+            try
+            {
+                var rpt = daPrestamo.getPrestar(paramss);
+                return Ok(rpt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
